2970faa3c4f2afee57c5ec46c85417a4aa238b5c
Danna <danna.wa@kakaobrain.com>
2022-07-25 22:48:54 +0900

    feat: add authority

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
@@ -10,10 +11,48 @@ public class Unit : NetworkBehaviour
     [SerializeField] private UnityEvent onSelected = null;
     [SerializeField] private UnityEvent onDeSelected = null;
 
+    public static event Action<Unit> ServerOnUnitSpawned; 
+    public static event Action<Unit> ServerOnUnitDespawned; 
+    
+    public static event Action<Unit> AuthorityOnUnitSpawned; 
+    public static event Action<Unit> AuthorityOnUnitDespawned; 
+
     public UnitMovement GetUnitMovement()
     {
         return unitMovement;
     }
+
+    #region Client
+
+    public override void OnStartClient()
+    {
+        if (!isClientOnly || !hasAuthority) { return; }
+        
+        AuthorityOnUnitSpawned.Invoke(this);
+    }
+    
+    public override void OnStopClient()
+    {
+        if (!isClientOnly || !hasAuthority) { return; }
+        
+        AuthorityOnUnitDespawned.Invoke(this);
+    }
+
+    #endregion
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        ServerOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopServer()
+    {
+        ServerOnUnitDespawned?.Invoke(this);
+    }
+
+    #endregion
 
     #region Client
 
