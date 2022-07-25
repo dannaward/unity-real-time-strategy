e7f4495c2c1ece87330aa3dd01b1fb966858d44e
Danna <danna.wa@kakaobrain.com>
2022-07-26 01:55:35 +0900

    feat: add selection box ui

--- Assets/Scripts/Units/UnitSelectionHandler.cs
+++ Assets/Scripts/Units/UnitSelectionHandler.cs
@@ -1,13 +1,20 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class UnitSelectionHandler : MonoBehaviour
 {
-    [SerializeField] private LayerMask layerMask = new LayerMask();
+    [SerializeField] private RectTransform unitSelectionArea = null;
     
+    [SerializeField] private LayerMask layerMask = new LayerMask();
+
+    private Vector2 startPosition;
+    
+    private RTSPlayer player;
     private Camera mainCamera;
 
     public List<Unit> SelectedUnits { get; } = new List<Unit>();
@@ -15,25 +22,52 @@ public class UnitSelectionHandler : MonoBehaviour
     private void Start()
     {
         mainCamera = Camera.main;
+        player = NetworkClient.connection.identity.GetComponent<RTSPlayer>();
     }
 
     private void Update()
     {
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-            foreach (Unit selectedUnit in SelectedUnits)
-            {
-                selectedUnit.Deselect();
-            }
-            
-            SelectedUnits.Clear();
+            StartSelectionArea();
         } 
         else if (Mouse.current.leftButton.wasReleasedThisFrame)
         {
             ClearSelectionArea();
         }
+        else if (Mouse.current.leftButton.isPressed)
+        {
+            UpdateSelectionArea();
+        }
     }
 
+    private void StartSelectionArea()
+    {
+        foreach (Unit selectedUnit in SelectedUnits)
+        {
+            selectedUnit.Deselect();
+        }
+        
+        SelectedUnits.Clear();
+        
+        unitSelectionArea.gameObject.SetActive(true);
+
+        startPosition = Mouse.current.position.ReadValue();
+        
+        UpdateSelectionArea();
+    }
+
+    private void UpdateSelectionArea()
+    {
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+
+        float areaWidth = mousePosition.x - startPosition.x;
+        float areaHeight = mousePosition.y - startPosition.y;
+
+        unitSelectionArea.sizeDelta = new Vector2(Mathf.Abs(areaWidth), Mathf.Abs(areaHeight));
+        unitSelectionArea.anchoredPosition = startPosition + new Vector2(areaWidth / 2, areaHeight / 2);
+    }
+    
     private void ClearSelectionArea()
     {
         Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
