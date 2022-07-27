91ba31b312fc278568241adaf8cb2411347d7f55
Danna <danna.wa@kakaobrain.com>
2022-07-28 00:51:24 +0900

    feat: detect game over

--- Assets/Scripts/Neworking/RTSNetworkManager.cs
+++ Assets/Scripts/Neworking/RTSNetworkManager.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RTSNetworkManager : NetworkManager
 {
     [SerializeField] private GameObject unitSpawnerPrefab = null;
+    [SerializeField] private GameOverHandler gameOverHandlerPrefab = null;
 
     public override void OnServerAddPlayer(NetworkConnectionToClient conn)
     {
@@ -18,4 +20,15 @@ public class RTSNetworkManager : NetworkManager
 
         NetworkServer.Spawn(unitSpawnerInstance, conn);
     }
+    
+    public override void OnServerSceneChanged(string sceneName)
+    {
+        if(SceneManager.GetActiveScene().name.StartsWith("Scene_Map"))
+        {
+            GameOverHandler gameOverHandlerInstance = Instantiate(gameOverHandlerPrefab);
+
+            NetworkServer.Spawn(gameOverHandlerInstance.gameObject);
+        }
+    }
+
 }
