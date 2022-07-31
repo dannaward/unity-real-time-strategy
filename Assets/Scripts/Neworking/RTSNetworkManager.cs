75718a85f20f2cce94d587b5de9b3f0a17a21183
Danna <danna.wa@kakaobrain.com>
2022-07-31 18:44:53 +0900

    feat: add lobby logic

--- Assets/Scripts/Neworking/RTSNetworkManager.cs
+++ Assets/Scripts/Neworking/RTSNetworkManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
@@ -8,6 +9,23 @@ public class RTSNetworkManager : NetworkManager
 {
     [SerializeField] private GameObject unitSpawnerPrefab = null;
     [SerializeField] private GameOverHandler gameOverHandlerPrefab = null;
+    
+    public static event Action ClientOnConnected;
+    public static event Action ClientOnDisconnected;
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+        ClientOnConnected?.Invoke();
+    }
+
+    public override void OnClientDisconnect(NetworkConnection conn)
+    {
+        base.OnClientDisconnect(conn);
+
+        ClientOnDisconnected?.Invoke();
+    }
 
     public override void OnServerAddPlayer(NetworkConnectionToClient conn)
     {
@@ -20,13 +38,6 @@ public class RTSNetworkManager : NetworkManager
             UnityEngine.Random.Range(0f, 1f),
             UnityEngine.Random.Range(0f, 1f)
         ));
-
-        GameObject unitSpawnerInstance = Instantiate(
-            unitSpawnerPrefab,
-            conn.identity.transform.position,
-            conn.identity.transform.rotation);
-
-        NetworkServer.Spawn(unitSpawnerInstance, conn);
     }
     
     public override void OnServerSceneChanged(string sceneName)
